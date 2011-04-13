0fc4c0c358473661ccbbff65fc295ff2dc3dd2a9
Andrew Vida <andrewjvida@gmail.com>
2011-04-13 08:33:48 -0400

    updating query test and README

--- company_query_test.rb
+++ company_query_test.rb
@@ -1,5 +1,7 @@
 require 'company.rb'
 
+# Find the company with the primary key (CompanyID) 3.
 company = Company.find(3)
+# => #<CompanyID: 3, company_name: => "My Test Company">
 puts company.inspect
 
